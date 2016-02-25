58d9b2380a5ad9c4d61256b7e82fd45d71b7cb86
Will Tabor <tabor.will@gmail.com>
2016-02-25 16:48:05 +0000

    Add password resets & email config

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -10,7 +10,8 @@ class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at https://rails-tutorial-tabor.c0.io/rails/mailers/user_mailer/password_reset
   def password_reset
-    UserMailer.password_reset
+    user = User.first
+    user.reset_token = User.new_token
+    UserMailer.password_reset(user)
   end
-
 end
