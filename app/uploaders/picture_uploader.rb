9544767831784055f0e8e107f9fdf84de9a1c07a
Will Tabor <tabor.will@gmail.com>
2016-04-14 13:21:43 +0000

    Add User Microposts

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,10 +1,10 @@
 class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick 
-  process resize_to_limit: [400, 400]
+  process resize_to_limit: [400,400]
   
   if Rails.env.production?
     storage :fog
-  else
+  else 
     storage :file
   end
 
